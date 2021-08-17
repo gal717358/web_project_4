805d3fc3aa1671cef6c64e2f847d81da6c105602
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-08-17 18:44:49 +0300

    first submition

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Project 4: Around The U.S.
 
-Link you my project : https://gal717358.github.io/web_project_4/
+![Around the U.S](https://drive.google.com/uc?export=view&id=1zkr1Zwfp2ddCDaS2WLWO5D_1bQUfbOaE)
+
+[Link to the Project](https://gal717358.github.io/web_project_4/)
+
---
+++ scripts.js
@@ -0,0 +1,32 @@
+let modal= document.querySelector('.modal')
+let modalClose=document.querySelector('.modal__opaned')
+let modalContainer = document.querySelector('.modal__container')
+let profileEditBtn = document.querySelector('.profile__edit-btn')
+let closeButton = document.querySelector('.modal__close-btn')
+let formElement = document.querySelector('.form')
+let profileName = document.querySelector('.profile__title')
+let profileJob = document.querySelector('.profile__job')
+let inputName = document.querySelector('.form__text-input_name')
+let inputJob = document.querySelector('.form__text-input_job')
+let submitBtn = document.querySelector('.form__submit-btn')
+
+function profileEditor(){
+if (modal.classList.contains('modal__opened')){
+    modal.classList.remove('modal__opened');}
+    else {modal.classList.add('modal__opened');}
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+
+}
+
+profileEditBtn.addEventListener('click',profileEditor );
+closeButton.addEventListener('click',profileEditor );
+
+function handleFormSubmit(evt){
+    evt.preventDefault();
+    profileName.textContent = `${inputName.value}`;
+    profileJob.textContent = `${inputJob.value}`;
+    modal.classList.add('modal__opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
