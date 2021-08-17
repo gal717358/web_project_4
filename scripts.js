667ac2cf6b8537e55c9b97d388069ce97c3f8aab
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-08-17 19:02:05 +0300

    First Submit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let modal= document.querySelector('.modal')
-let modalClose=document.querySelector('.modal__opaned')
+let modalClose=document.querySelector('.modal_opaned')
 let modalContainer = document.querySelector('.modal__container')
 let profileEditBtn = document.querySelector('.profile__edit-btn')
 let closeButton = document.querySelector('.modal__close-btn')
@@ -11,9 +11,9 @@ let inputJob = document.querySelector('.form__text-input_job')
 let submitBtn = document.querySelector('.form__submit-btn')
 
 function profileEditor(){
-if (modal.classList.contains('modal__opened')){
-    modal.classList.remove('modal__opened');}
-    else {modal.classList.add('modal__opened');}
+if (modal.classList.contains('modal_opened')){
+    modal.classList.remove('modal_opened');}
+    else {modal.classList.add('modal_opened');}
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
 
@@ -26,7 +26,7 @@ function handleFormSubmit(evt){
     evt.preventDefault();
     profileName.textContent = `${inputName.value}`;
     profileJob.textContent = `${inputJob.value}`;
-    modal.classList.add('modal__opened');
+    modal.classList.add('modal_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
