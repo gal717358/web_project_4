ca630d406926f67187a94e022e3795d25106bc61
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-08-18 14:28:31 +0300

    after repairs

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 [Link to the Project](https://gal717358.github.io/web_project_4/)
 
+the project's name; Around The U.S.
+a description of the project: The project shown a profile of social media platform of a famouse explorer that share with everybody
+about nice places to travel.
+a description of technologies and techniques used: In this project we used with a combination of HTML CSS and JS in order to make the profile responsive and informativ and to make him dinamic by this that we can actually to change inputes and to bring buttons to live.
+
---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+let modal = document.querySelector(".modal");
+let modalClose = document.querySelector(".modal_closed");
+let modalContainer = document.querySelector(".modal__container");
+let profileEditBtn = document.querySelector(".profile__edit-btn");
+let closeButton = document.querySelector(".modal__close-btn");
+let formElement = document.querySelector(".form");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__job");
+let inputName = document.querySelector(".form__text-input_type_name");
+let inputJob = document.querySelector(".form__text-input_type_job");
+let submitBtn = document.querySelector(".form__submit-btn");
+
+function profileEditor() {
+    if (modal.classList.toggle("modal_closed"));
+    else inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+}
+
+profileEditBtn.addEventListener("click", profileEditor);
+closeButton.addEventListener("click", profileEditor);
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = `${inputName.value}`;
+    profileJob.textContent = `${inputJob.value}`;
+    modal.classList.add("modal_closed");
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
