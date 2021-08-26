3601d76497006b10590e4b22e931f47b4fd4e940
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-08-26 20:13:55 +0300

    hop it is the last

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,7 +23,7 @@ const element = templateElement.cloneNode(true);
 
 //pop up
 const modalPopup = document.querySelector(".modal_type_pop-up");
-const closePopupBtn = document.querySelector(".modal__close_popup");
+const closePopupBtn = document.querySelector(".modal__close-popup");
 const modalpopupContainer = document.querySelector("modal__container_type_popup");
 const largeImage = document.querySelector(".modal__img");
 const popupTitle = document.querySelector(".modal__title");
