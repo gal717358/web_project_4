4e7e980f2f719f28c46feac76097c5106f9a6002
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-08-26 16:07:30 +0300

    check again

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,3 @@
-
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal_closed");
 const modalContainer = document.querySelector(".modal__container");
@@ -22,75 +21,89 @@ const templateElement = document.querySelector("#element-template").content.quer
 const elementsBlock = document.querySelector(".elements");
 const element = templateElement.cloneNode(true);
 
+//pop up
 const modalPopup = document.querySelector(".modal_type_pop-up");
-const closePopupBtn = document.querySelector(".modal__close-btn");
-const modalpopupContainer = document.querySelector(".modal__container");
+const closePopupBtn = document.querySelector(".modal__close_type_popup");
+const modalpopupContainer = document.querySelector("modal__container_type_popup");
+const largeImage = document.querySelector(".pop-up__img");
+const popupTitle = document.querySelector(".pop-up__title");
 
-
-const elementDescription = document.querySelector(".element__description");
 const elementLikeActive = document.querySelector(".element__like-btn_active");
 const url = document.querySelector(".form__text-input_type_img-element");
 const title = document.querySelector(".form__text-input_type_name-element");
 
-
-
 const initialElements = [
     {
-      name: "Yosemite Valley",
-      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
     },
     {
-      name: "Lake Louise",
-      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
     },
     {
-      name: "Bald Mountains",
-      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
     },
     {
-      name: "Latemar",
-      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
     },
     {
-      name: "Vanoise National Park",
-      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
     },
     {
-      name: "Lago di Braies",
-      link: "https://code.s3.yandex.net/web-code/lago.jpg"
-    }
-  ]; 
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+];
 
-  function addElement (data) {
-  
+function addElement(el) {
     const element = templateElement.cloneNode(true);
     const elementTitle = element.querySelector(".element__title");
     const elementImage = element.querySelector(".element__image");
-  
-    elementTitle.textContent = data.name;
-    elementImage.setAttribute('src',data.link);
-    elementImage.setAttribute('alt',data.name);
+    const elementLikebtn = element.querySelector(".element__like-btn");
+    const elementDeletebtn = element.querySelector(".element__delete");
+
+    elementTitle.textContent = el.name;
+    elementImage.setAttribute("src", el.link);
+    elementImage.setAttribute("alt", el.name);
+
+    elementLikebtn.addEventListener("click", () => {
+        elementLikebtn.classList.toggle("element__like-btn_active");
+    });
+    elementDeletebtn.addEventListener("click", () => {
+        element.remove();
+    });
+    elementImage.addEventListener("click", () => {
+        modalPopup.classList.remove("modal_closed");
+        popupTitle.textContent = el.name;
+        popupTitle.setAttribute("alt", el.name);
+        largeImage.setAttribute("src", el.link);
+    });
+
     elementsBlock.prepend(element);
-  }
-  
-  initialElements.forEach(addElement)
+}
 
-  submitAddCard.addEventListener("click", function (e) {
+initialElements.forEach(addElement);
+
+submitAddCard.addEventListener("click", function (e) {
     e.preventDefault();
-
-    addElement(url.value, title.value);
+    addElement({ name: title.value, link: url.value });
     modalAddelement.classList.add("modal_closed");
 });
 
-
-
-closeButton.addEventListener('click', () => {
-    modal.classList.add("modal_closed");
-});
-profileEditBtn.addEventListener('click', () => {
+profileEditBtn.addEventListener("click", () => {
     modal.classList.remove("modal_closed");
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
+});
+closeButton.addEventListener("click", () => {
+    modal.classList.add("modal_closed");
+});
+closePopupBtn.addEventListener("click", () => {
+    modalPopup.classList.add("modal_closed");
 });
 
 function handleFormSubmit(evt) {
@@ -99,35 +112,11 @@ function handleFormSubmit(evt) {
     profileJob.textContent = `${inputJob.value}`;
     modal.classList.add("modal_closed");
 }
-
 formElement.addEventListener("submit", handleFormSubmit);
 
-addCardbtn.addEventListener('click', () => {
-  modalAddelement.classList.remove("modal_closed");
+addCardbtn.addEventListener("click", () => {
+    modalAddelement.classList.remove("modal_closed");
 });
-closeAddBtn.addEventListener('click', () => {
-  modalAddelement.classList.add("modal_closed");
+closeAddBtn.addEventListener("click", () => {
+    modalAddelement.classList.add("modal_closed");
 });
-
-
-const removeEl = (evt) => {
-    evt.target.closest(".element").remove();
-};
-
-const LikeElement = (evt) => {
-    evt.target.classList.toggle("element__like-btn_active");
-};
-
-function openPopup(){
-    modalPopup.classList.remove("modal_closed");
-}
-function closePopup(){
-    modalPopup.classList.add("modal_closed");
-}
-
-
-
-
-
-
-
