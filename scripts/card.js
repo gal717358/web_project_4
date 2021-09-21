15d2cd8e5ceb96df2f347f699854e087c56a786c
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-09-21 14:04:56 +0300

    import check

---
+++ scripts/card.js
@@ -0,0 +1,73 @@
+const initialElements = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+];
+
+class Card{
+    constructor(data, cardSelector,image){
+        this._name= data.name;
+        this._link = data.link;
+        this._cardSelector = cardSelector;
+    }
+
+    _getTemplate() {
+        const templateElement = document.querySelector("#element-template").content     
+        .querySelector(".element")
+        .cloneNode(true);
+
+        return templateElement;
+      }
+      generateCard() {
+        this._element = this._getTemplate();
+        this._setEventListieners();
+        
+        this._element.querySelector(".element__image").src = this._link;
+        this._element.querySelector(".element__image").setAttribute = `alt(${this._link})`;
+        this._element.querySelector(".element__title").textContent = this._name;
+
+
+        return this._element;
+         }    
+
+        _setEventListieners(){
+            const elementLikebtn = this._element.querySelector(".element__like-btn");
+            const elementDeletebtn = this._element.querySelector(".element__delete");
+            const elementImage = this._element.querySelector(".element__image");
+            elementLikebtn.addEventListener("click", () => {
+                elementLikebtn.classList.toggle("element__like-btn_active");
+            });
+            elementDeletebtn.addEventListener("click", () => {
+                this._element.remove();
+            });
+            elementImage.addEventListener("click", () => {
+                openPopUp(modalPopup);
+                popupTitle.textContent = this._name;
+                popupTitle.setAttribute("alt",  `alt(${this._link})`);
+                largeImage.setAttribute("src", this._link);
+            });
+        }           
+}
+
+export {Card, initialElements};
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+import {Card, initialElements} from "./card.js"
 
 //edit profile
 const modal = document.querySelector(".modal");
@@ -31,77 +32,7 @@ const popupTitle = document.querySelector(".modal__title");
 const elementLikeActive = document.querySelector(".element__like-btn_active");
 const url = document.querySelector(".form__text-input_type_img-element");
 const title = document.querySelector(".form__text-input_type_name-element");
-const initialElements = [
-    {
-        name: "Yosemite Valley",
-        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-    },
-    {
-        name: "Lake Louise",
-        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-    },
-    {
-        name: "Bald Mountains",
-        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-    },
-    {
-        name: "Latemar",
-        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-    },
-    {
-        name: "Vanoise National Park",
-        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-    },
-    {
-        name: "Lago di Braies",
-        link: "https://code.s3.yandex.net/web-code/lago.jpg",
-    },
-];
 
-class Card{
-    constructor(data, cardSelector,image){
-        this._name= data.name;
-        this._link = data.link;
-        this._cardSelector = cardSelector;
-    }
-
-    _getTemplate() {
-        const templateElement = document.querySelector("#element-template").content     
-        .querySelector(".element")
-        .cloneNode(true);
-
-        return templateElement;
-      }
-      generateCard() {
-        this._element = this._getTemplate();
-        this._setEventListieners();
-        
-        this._element.querySelector(".element__image").src = this._link;
-        this._element.querySelector(".element__image").setAttribute = `alt(${this._link})`;
-        this._element.querySelector(".element__title").textContent = this._name;
-
-
-        return this._element;
-         }    
-
-        _setEventListieners(){
-            const elementLikebtn = this._element.querySelector(".element__like-btn");
-            const elementDeletebtn = this._element.querySelector(".element__delete");
-            const elementImage = this._element.querySelector(".element__image");
-            elementLikebtn.addEventListener("click", () => {
-                elementLikebtn.classList.toggle("element__like-btn_active");
-            });
-            elementDeletebtn.addEventListener("click", () => {
-                this._element.remove();
-            });
-            elementImage.addEventListener("click", () => {
-                openPopUp(modalPopup);
-                popupTitle.textContent = this._name;
-                popupTitle.setAttribute("alt",  `alt(${this._link})`);
-                largeImage.setAttribute("src", this._link);
-            });
-        }           
-}
 
 initialElements.forEach((ele) => {
 	const card = new Card(ele);
