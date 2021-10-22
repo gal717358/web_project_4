965c4e380316fad0373af10e77999af45a3e44eb
gal717358 <84976668+gal717358@users.noreply.github.com>
2021-10-22 17:59:36 +0300

    after reparis3

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -6,6 +6,7 @@ export default class PopupWithImage extends Popup {
     const cardImage = evt.target;
 
     this._popupElement.querySelector(".modal__img").src = cardImage.src;
+    this._popupElement.querySelector(".modal__img").alt= cardImage.alt;
     this._popupElement.querySelector(".modal__title").textContent =
       cardImage.alt;
   };
